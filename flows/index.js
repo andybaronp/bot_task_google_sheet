b037ee03739a933da04e1fef5c0fd97bcc65bee3
Jhoandris B <andybaronp@gmail.com>
2023-10-06 10:25:13 -0500

    refactor

---
+++ flows/index.js
@@ -0,0 +1,97 @@
+const { addKeyword } = require("@bot-whatsapp/bot")
+const { getTask, updatedTask, saveTask } = require("../utils/funtionsTask")
+
+const flowEndChat = addKeyword('end').addAnswer(
+  'Registro *cancelado* por falta de actividad del cliente.\n Puedes iniciar el registro nuevamente o consultar pentieste enviando *Lista*',
+)
+
+const flowRegisterTask = addKeyword(['registrar', 'registra', 'registro'])
+  .addAnswer('Ok! vamos a registrar una tarea', { delay: 2000 })
+  .addAnswer(
+    'Dime tu tarea pendiente',
+    { capture: true, idle: 35000, delay: 2000 },
+    async (ctx, { flowDynamic, gotoFlow }) => {
+      if (ctx?.idleFallBack) {
+        return gotoFlow(flowEndChat)
+      }
+      task = { task: ctx.body, from: ctx.from }
+      const res = await saveTask(task)
+      if (res) {
+        await flowDynamic([
+          {
+            body: `Registrada`,
+          },
+        ])
+      } else {
+        await flowDynamic([
+          {
+            body: `*Hubo un error*`,
+          },
+        ])
+      }
+      flowDynamic()
+    },
+  )
+
+const flowEditTask = addKeyword(['terminar', 'completar']).addAnswer(
+  'Ok! indica el *ID* de la tarea',
+
+  { capture: true, idle: 35000, delay: 2000 },
+  async (ctx, { flowDynamic, gotoFlow }) => {
+    if (ctx?.idleFallBack) {
+      return gotoFlow(flowEndChat)
+    }
+    let task = { id: ctx.body, from: ctx.from }
+    const { status, message } = await updatedTask(task)
+    if (status) {
+      await flowDynamic([
+        {
+          body: message,
+        },
+      ])
+    } else {
+      await flowDynamic([
+        {
+          body: `*Hubo un error*`,
+        },
+      ])
+    }
+    flowDynamic()
+  },
+)
+const flowListTask = addKeyword([
+  'listar',
+  'lista',
+  'pendiente',
+  'tareas',
+]).addAnswer(
+  'Ok! vamos a listar tus pendientes',
+  { delay: 2000 },
+  async (ctx, { flowDynamic }) => {
+    const tasksList = await getTask(ctx)
+    if (tasksList.length === 0) {
+      return flowDynamic([
+        { body: `*${ctx.pushName}* no tienes tareas pendientes` },
+      ])
+    }
+    flowDynamic([
+      { body: `*${ctx.pushName}* tus pendientes son:`, delay: 2000 },
+    ])
+    const taks = tasksList.map((dato) => ({
+      body: `*ID:* ${dato.id}\n*Tarea:* ${dato.task}\n*Registrada:* ${dato.date}`,
+      delay: 3000,
+    }))
+    await flowDynamic(taks)
+    flowDynamic([{ body: `*${ctx.pushName}* esas son tus tareas pendientes✅\nPuedes completarlas utilizando el comando *completar* `, delay: 3000 }])
+  },
+)
+
+const flowStartChat = addKeyword(['start', 'iniciar', 'inicio', 'comandos', '/']).addAnswer('📃La lista de comandos son:\n \n👉 *Listar* para ver las tareas pendientes\n👉 *Completar* para marcar como completa una tarea\n👉 *Registrar* para registrar *una tarea*', {
+  media: 'https://i.imgur.com/xq9gKnAm.jpg'
+})
+const flowEndChatSay = addKeyword(['adios', 'gracias']).addAnswer('Nos vemos 🤘', {
+  media: 'https://i.imgur.com/og03n40m.jpg'
+})
+
+
+module.exports = { flowStartChat, flowEndChatSay, flowEndChat, flowRegisterTask, flowEditTask, flowListTask }
---
+++ utils/funtionsTask.js
@@ -0,0 +1,84 @@
+// autho
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const RESPONSES_SHEET_ID = process.env.SHEET_ID //Aquí pondras el ID de tu hoja de Sheets
+const { JWT } = require('google-auth-library')
+const serviceAccountAuth = new JWT({
+  email: process.env.CLIENT_EMAIL,
+  key: process.env.PRIVATE_KEY,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+})
+const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID, serviceAccountAuth)
+
+
+const { formatDate } = require("./formatDate")
+
+async function saveTask(task) {
+  let rows = [
+    {
+      status: 'pendiente',
+      date: formatDate(new Date()),
+      task: task.task,
+      from: task.from,
+    },
+  ]
+  try {
+    await doc.loadInfo()
+    let sheet = doc.sheetsByIndex[1]
+    for (let index = 0; index < rows.length; index++) {
+      const row = rows[index]
+      await sheet.addRow(row)
+    }
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+async function getTask(ctx) {
+  let listTask = []
+  try {
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1]
+    const rows = await sheet.getRows()
+    for (let index = 0; index < rows.length; index++) {
+      const row = rows[index]
+      if (row._rawData[0] === ctx.from && row._rawData[2] === 'pendiente') {
+        listTask.push({
+          'task': row._rawData[1],
+          'date': row._rawData[3],
+          'id': row._rowNumber,
+        })
+      }
+    }
+    return listTask
+  } catch (error) {
+    return false
+  }
+}
+
+async function updatedTask(task) {
+  try {
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1]
+    const rows = await sheet.getRows()
+    const rowsLength = rows.find(row => row._rowNumber === Number(task.id))
+    if (rowsLength) {
+      if (rowsLength._rawData[0] === task.from && rowsLength._rowNumber === Number(task.id) && rowsLength._rawData[2] === 'pendiente') {
+        rowsLength._rawData[2] = 'completada'
+        await rowsLength.save()
+        return { status: true, message: 'Tarea completada' }
+      }
+      else {
+        return { status: true, message: 'No se encontro la tarea \nIntenta listando tus tareas con *Listar*' }
+      }
+
+    } else {
+      return { status: true, message: 'No se encontro la tarea \nIntenta listando tus tareas con *Listar*' }
+    }
+  } catch (error) {
+    return false
+  }
+}
+
+
+module.exports = { saveTask, getTask, updatedTask }
